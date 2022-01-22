dad7958e43d8a18ad0e4435a18c71bb171283350
Yash Agarwal <yash18082002@gmail.com>
2022-01-22 11:58:38 +0530

    Complete game logic

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// const screen = document.querySelector('.container');
-// screen.addEventListener('click', function () {
-//     screen.classList.toggle('clicked');
-//     console.log(screen.className);
-// })
 let num = 0;
+let win = 0;
 const isGameOver = function () {
     if (num === 9) {
+        return 1;
+    }
+    else if (win >= 1) {
         return 1;
     }
     else {
@@ -15,6 +14,7 @@ const isGameOver = function () {
 const move = function () {
     if (isGameOver() === 1) {
         alert('Game is over, reload the page to play a new game.');
+        return;
     }
     if (this.innerText === 'X' || this.innerText === 'O') {
         return;
@@ -27,26 +27,37 @@ const move = function () {
         this.innerText = 'O';
         num++;
     }
+    checkWin();
 }
 const boxes = document.querySelectorAll('.box');
 boxes.forEach(function (e) {
     e.addEventListener('click', move);
 })
 
-// const checkWin = function(){
-//     let winConditions = [
-//         boxes[0].innerText + boxes[1].innerText + boxes[2].innerText, 
-//         boxes[3].innerText + boxes[4].innerText + boxes[5].innerText, 
-//         boxes[6].innerText + boxes[7].innerText + boxes[8].innerText, 
-//         boxes[0].innerText + boxes[3].innerText + boxes[6].innerText, 
-//         boxes[1].innerText + boxes[4].innerText + boxes[7].innerText, 
-//         boxes[2].innerText + boxes[5].innerText + boxes[8].innerText, 
-//         boxes[0].innerText + boxes[4].innerText + boxes[8].innerText,
-//         boxes[2].innerText + boxes[4].innerText + boxes[6].innerText
-//     ];
-//     winConditions.forEach(function(e){
-//         if(e === 'XXX'){
-//             return 1;
-//         }
-//     });
-// }
+const checkWin = function () {
+    let winConditions = [
+        { value: boxes[0].innerText + boxes[1].innerText + boxes[2].innerText, tiles: [0, 1, 2] },
+        { value: boxes[3].innerText + boxes[4].innerText + boxes[5].innerText, tiles: [3, 4, 5] },
+        { value: boxes[6].innerText + boxes[7].innerText + boxes[8].innerText, tiles: [6, 7, 8] },
+        { value: boxes[0].innerText + boxes[3].innerText + boxes[6].innerText, tiles: [0, 3, 6] },
+        { value: boxes[1].innerText + boxes[4].innerText + boxes[7].innerText, tiles: [1, 4, 7] },
+        { value: boxes[2].innerText + boxes[5].innerText + boxes[8].innerText, tiles: [2, 5, 8] },
+        { value: boxes[0].innerText + boxes[4].innerText + boxes[8].innerText, tiles: [0, 4, 8] },
+        { value: boxes[2].innerText + boxes[4].innerText + boxes[6].innerText, tiles: [2, 4, 6] }
+    ];
+    winConditions.forEach(function (e) {
+        if (e.value === 'XXX') {
+            win++;
+            e.tiles.forEach(function (i) {
+                boxes[i].classList.toggle('cyan');
+            })
+        }
+        if (e.value === 'OOO') {
+            win++;
+            e.tiles.forEach(function (i) {
+                boxes[i].classList.toggle('yellow');
+            })
+        }
+    });
+    return win;
+}
